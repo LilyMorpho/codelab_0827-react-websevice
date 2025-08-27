b09f442e46fa7ee171dd27310542fe4e0e29e347
LilyMorpho <ikaros9903@daum.net>
2025-08-27 16:27:27 +0900

    reset ShopPage

--- src/components/ShopPage.jsx
+++ src/components/ShopPage.jsx
@@ -1,11 +1,1 @@
-import { Container } from "@mui/material"
 
-import Headwarpper from "../Header/headerWrapper"
-
-export default function Containers() {
-  return (
-    <Container>
-      <Headwarpper />
-    </Container>
-  )
-}
